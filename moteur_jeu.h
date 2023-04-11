bf624d9aa191f13b269e27b53cab77a5b4289e18
Théotime <potard.theotime@gmail.com>
2023-04-11 15:23:48 +0200

    Ajout nouvelles struct et fonctions dans moteur_jeu.h

--- moteur_jeu.h
+++ moteur_jeu.h
@@ -6,6 +6,21 @@ typedef struct map {
     int cases[TAILLE_MAP][TAILLE_MAP];
 } map_t;
 
+typedef struct position {
+    int x;
+    int y;
+} pos_t;
+
+typedef struct joueur{
+    int skin;
+    pos_t position_joueur;
+    int nb_bombes;
+    int nb_vies;
+    int vitesse;
+} joueur_t;
+
 void affichage_carte(map_t carte);
 
 void map_avec_fichier (char* nom_fichier, map_t* map);
+
+void init_joueur(int nb_joueurs, char* nom_fichier);
